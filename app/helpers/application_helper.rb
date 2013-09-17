3bd622ccabb9ddb7721d8fce341ff0533e5f6cd5
Steve Fisher <stevof@yahoo.com>
2013-09-17 11:59:27 -0700

    applied 'Adequately' theme from FreeCssTemplates.org

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,20 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def is_active?(page_name, active_class_name = "current_page_item")
+    # this should be called form a view. if we're on the current page, return a css class name
+    # (that we can use to highlight the active page in a nav menu, for example)
+
+    puts
+    puts "#is_active?"
+    puts "params[:action] == #{params[:action]}"
+    puts "page_name == #{page_name}"
+    puts "current_page?(page_name) == #{current_page?(page_name)}"
+    puts "params:"
+    puts params.inspect
+    puts
+
+    active_class_name if params[:controller] == page_name
+  end  
 end
